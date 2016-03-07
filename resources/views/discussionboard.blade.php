69b39377f0f26da4f367e73b4b577681737c4742
Rachel Dorn <rrdorn@gmail.com>
2016-03-07 12:29:17 -0500

    Added the forum for the discussion board

--- resources/views/discussionboard.blade.php
+++ resources/views/discussionboard.blade.php
@@ -8,7 +8,20 @@
                     <div class="panel-heading"><center><h1>Discussion Board</h1></center></div>
 
                     <div class="panel-body">
-                        Discussion Board goes here.
+                        <iframe id="forum_embed"
+                                src="javascript:void(0)"
+                                scrolling="no"
+                                frameborder="0"
+                                width="900"
+                                height="700">
+                        </iframe>
+
+                        <script type="text/javascript">
+                            document.getElementById("forum_embed").src =
+                                    "https://groups.google.com/forum/embed/?place=forum/fEMR" +
+                                    "&showsearch=true&showpopout=true&parenturl=" +
+                                    encodeURIComponent(window.location.href);
+                        </script>
                     </div>
                 </div>
             </div>
