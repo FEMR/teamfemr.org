0a3014a71c799324aa6e3731a0f4552365008df8
Rachel Dorn <rrdorn@gmail.com>
2016-04-11 13:58:01 -0400

    Updated fear pages to match our pages -RD

--- resources/views/home0.blade.php
+++ resources/views/home0.blade.php
@@ -2,68 +2,77 @@
 
 @section('content')
 <div class="container">
-<div class="content">
-    <div class="content_top margins">
-        <h1>A fast EMR solution for transient medical teams.</h1>
-        <p>We are using open source technology to promote data driven communciation in low resource settings.</p>
-	<a class="button" href="./other/Annual_Report_2014_2015.pdf">First Annual Report</a>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading"><center><h1>HOME</h1></center></div>
 
-    </div>
-    <div class="content_top slack-info">
+                    <div class="panel-body">
+                        <div class="content_top margins">
+                            <h1>A fast EMR solution for transient medical teams.</h1>
+                            <p>We are using open source technology to promote data driven communciation in low resource settings.</p>
+                            <a class="button" href="./other/Annual_Report_2014_2015.pdf">First Annual Report</a>
 
-        <h2>Slack</h2>
+                        </div>
+                        <div class="content_top slack-info">
 
-        <p>Interested in development? Join the fEMR discussion in our slack chat! </p>
+                            <h2>Slack</h2>
 
-        <a class="button slack-button" href="/slack.html">Join Our Slack!</a>
-    </div>
-    <div class="content_middle">
-        <div class="tiles">
-            <div class="tiles_firstrow">
-                <div class="tile1">
-                    <h2>Team fEMR!</h2>
-                    <div class="tileContent">
-                        <img src="/images/screenshots/team_femr.png" alt="Team fEMR">
-                        <p>We are students, doctors, and engineers. More importantly we are volunteers helping to provide the best healthcare possible to people who are often without access to life's basic necessities.</p>
+                            <p>Interested in development? Join the fEMR discussion in our slack chat! </p>
+
+                            <a class="button slack-button" href="/slack.html">Join Our Slack!</a>
+                        </div>
+                        <div class="content_middle">
+                    <div class="tiles">
+                        <div class="tiles_firstrow">
+                            <div class="tile1">
+                                <h2>Team fEMR!</h2>
+                                <div class="tileContent">
+                                    <img src="/images/screenshots/team_femr.png" alt="Team fEMR">
+                                    <p>We are students, doctors, and engineers. More importantly we are volunteers helping to provide the best healthcare possible to people who are often without access to life's basic necessities.</p>
+                                </div>
+                            </div>
+                            <div class="tile2">
+                                <h2>Who is fEMR for?</h2>
+                                <div class="tileContent">
+                                    <img src="/images/screenshots/who_is_femr_for.png" alt="Who is fEMR for">
+                                    <p>We have designed fEMR for transient medical teams who need a fast and easy way to record patient information. Most of our users work in settings with limited infrastructure.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tiles_secondrow">
+                            <div class="tile3">
+                                <h2>Open Source</h2>
+                                <div class="tileContent">
+                                    <img src="/images/screenshots/open_source.png" alt="Open Source">
+                                    <p>fEMR is both free and open source. As a responsive web application, it can be used on any smart device.</p>
+                                    <a href="https://github.com/femr/femr" target="_blank" class="devButton" id="home_repoBtn">Repository</a>
+                                    <a href="https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!forum/team-femr" target="_blank" class="devButton" id="home_repoBtn">Mailing List</a>
+                        <a href="https://teamfemr.atlassian.net" target="_blank" class="devButton" id="jiraButton">Backlog</a>
+                                    <a href="/slack.html" target="_blank" class="devButton" id="slackButton">Slack</a>
+                                    <!-- <a class="devButton" id="ircButton">IRC</a> -->
+                                </div>
+                            </div>
+                            <div class="tile4">
+                                <h2>Demo</h2>
+                                <div class="tileContent">
+                                    <img src="/images/screenshots/demo.png" alt="Demo">
+                                    <p>
+                                        We currently have a development version of fEMR available for testing <a href="http://femr.teamfemr.org">here.</a>
+                                        <span id="username">Username: visitor</span>
+                                        <span id="password">Password: femr1</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="tile2">
-                    <h2>Who is fEMR for?</h2>
-                    <div class="tileContent">
-                        <img src="/images/screenshots/who_is_femr_for.png" alt="Who is fEMR for">
-                        <p>We have designed fEMR for transient medical teams who need a fast and easy way to record patient information. Most of our users work in settings with limited infrastructure.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="tiles_secondrow">
-                <div class="tile3">
-                    <h2>Open Source</h2>
-                    <div class="tileContent">
-                        <img src="/images/screenshots/open_source.png" alt="Open Source">
-                        <p>fEMR is both free and open source. As a responsive web application, it can be used on any smart device.</p>
-                        <a href="https://github.com/femr/femr" target="_blank" class="devButton" id="home_repoBtn">Repository</a>
-                        <a href="https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!forum/team-femr" target="_blank" class="devButton" id="home_repoBtn">Mailing List</a>
-			<a href="https://teamfemr.atlassian.net" target="_blank" class="devButton" id="jiraButton">Backlog</a>
-                        <a href="/slack.html" target="_blank" class="devButton" id="slackButton">Slack</a>
-                        <!-- <a class="devButton" id="ircButton">IRC</a> -->
-                    </div>
-                </div>
-                <div class="tile4">
-                    <h2>Demo</h2>
-                    <div class="tileContent">
-                        <img src="/images/screenshots/demo.png" alt="Demo">
-                        <p>
-                            We currently have a development version of fEMR available for testing <a href="http://femr.teamfemr.org">here.</a>
-                            <span id="username">Username: visitor</span>
-                            <span id="password">Password: femr1</span>
-                        </p>
+                    <div class="content_bottom">
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="content_bottom">
-    </div>
-</div>
 </div>
 @endsection
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -2,50 +2,58 @@
 
 @section('content')
     <div class="container">
-    <div class="content">
-    <div class="content_top margins">
-        <h1>A fast EMR solution for transient medical teams.</h1>
-        <p>We are using open source technology to promote data driven communciation in low resource settings.</p>
-	<a href="./public/other/Annual_Report_2014_2015.pdf">First Annual Report</a>
-    </div>
-    <div class="content_middle">
-        <div class="tiles margins">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><center><h1>NEWS</h1></center></div>
 
-            <ul id="newsList">
-                <li> 
-                    <a href="http://www.youtube.com/watch?v=Zppwhc2vHgo">
-                        Wayne State University promo campaign video featuring fEMR
-                    </a>
-                </li>
-                <li> 
-                    <a href="http://www.improvewsu.org/Articles/tabid/90/ID/6/WSU-Students-Lead-Development-of-Electronic-Medical-Records-in-Haiti.aspx">
-                        Wayne State University, ImproveWSU.org, "WSU students lead development of electronic medical records in Haiti"
-                    </a>
-                </li>
-                <li> 
-                    <a href="http://www.emrandehr.com/tag/emergency-ehr/">                
-                        EMR & EHR Forum, Review by field expert, "fEMR targets pop-up clinics' needs"
-                    </a>
-                </li>
-                <li> 
-                    <a href="http://www.clickondetroit.com/news/live-in-the-d/detroit-college-students-aim-to-help-haiti-earthquake-victims/24959282">
-                        Live in the D, WDIV Detroit, news segment, "Detroit college students aim to help Haiti earthquake victims"
-                    </a>    
-                </li>
-                <li>
-                    <a href="http://prognosis.med.wayne.edu/article/wsu-computer-science-students-open-source-software-benefits-med-school-mission-trips">
-                        Wayne State University SOM Prognosis E-news, "WSU computer science students'     open source software benefits School of Medicine mission trips"
-                    </a> 
-                </li>
-                
-                
+                    <div class="panel-body">
+                    <div class="content_top margins">
+                        <h1>A fast EMR solution for transient medical teams.</h1>
+                        <p>We are using open source technology to promote data driven communciation in low resource settings.</p>
+                    <a href="./public/other/Annual_Report_2014_2015.pdf">First Annual Report</a>
+                    </div>
+                    <div class="content_middle">
+                        <div class="tiles margins">
 
-            </ul>
+                            <ul id="newsList">
+                                <li>
+                                    <a href="http://www.youtube.com/watch?v=Zppwhc2vHgo">
+                                        Wayne State University promo campaign video featuring fEMR
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://www.improvewsu.org/Articles/tabid/90/ID/6/WSU-Students-Lead-Development-of-Electronic-Medical-Records-in-Haiti.aspx">
+                                        Wayne State University, ImproveWSU.org, "WSU students lead development of electronic medical records in Haiti"
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://www.emrandehr.com/tag/emergency-ehr/">
+                                        EMR & EHR Forum, Review by field expert, "fEMR targets pop-up clinics' needs"
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://www.clickondetroit.com/news/live-in-the-d/detroit-college-students-aim-to-help-haiti-earthquake-victims/24959282">
+                                        Live in the D, WDIV Detroit, news segment, "Detroit college students aim to help Haiti earthquake victims"
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://prognosis.med.wayne.edu/article/wsu-computer-science-students-open-source-software-benefits-med-school-mission-trips">
+                                        Wayne State University SOM Prognosis E-news, "WSU computer science students'     open source software benefits School of Medicine mission trips"
+                                    </a>
+                                </li>
+
+
+
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="content_bottom">
+                    </div>
+                </div>
+                </body>
+            </div>
+            </div>
         </div>
     </div>
-    <div class="content_bottom">
-    </div>
-</div>
-</body>
-</div>
 @endsection
