aa1fd7dfe2792e426ed93c31f6cc5048a03582ef
Arooba <arooba@SSS-Mobile-Unit-3.local>
2016-04-13 16:34:43 -0400

    Removed some code that was not being used on Literature Bank page, documented the blade file

--- resources/views/literaturebank.blade.php
+++ resources/views/literaturebank.blade.php
@@ -2,36 +2,45 @@
 
 @section('content')
 
+                <!--This page is the display for the Literature Bank page-->
 
+                <!--Use bootstrap to integrate the web page, so that it looks similar to the other web pages-->
                 <div class="panel-heading"><center><h1>Literature Bank</h1></center></div>
                   <div class="panel-body">
                     <!--Link to Literature Bank Survey-->
                       <div><a href ="/litbanksurvey/create" class="btn btn-danger btn-sm">ADD RESOURCE</a>
                           <hr>
                       </div>
-                      {{--<h2><a href="litbanksurvey/create">Click here to add a resource</a></h2>--}}
-
-
                                 <!--Portion of web page where the links to resources are listed-->
 
+                      <!--Formatting of page-->
                     <h3>New Additions:</h3>
                     <div class="row">
+
+                        <!--Loop through the approved literature articles and format them so that only two literature article cards appear on one line-->
                         @foreach( $literatures as $id => $literature )
                             @if($id%2 == 1)
                                 <div class="row">
                                     @endif
+
+                                    <!--Use bootstrap to make cards fit in with the theme of the web page-->
                         <div class="col-md-6">
                             <div class="panel panel-default">
                                 <div class="panel-heading">
+                                    <!--Pull the titles of web pages and display them-->
                                     <a href="<?php echo $info[$id]->url ?>"><?php echo $info[$id]->title ?></a>
                                 </div>
                                  <br>
                                  <?php echo $info[$id]->description ?>
                                  <br>
 
+                                <!--Display scraped image-->
                                  <a class="img-responsive center-block" href="<?php echo $info[$id]->url ?>"><img src="<?php echo $info[$id]->image ?>" alt="Main image" style="width:200px;height:150px;"></a>
                                  <br>
+                                <!--Display scraped published date on site-->
                                  <?php echo $info[$id]->publishedDate ?>
+
+                                <!--Display file from file upload-->
                                  @if($literature->fileName)
                                       <h4><a href= {{$literature->fileName}}>Download Related Content</a></h4>
                                  @endif
@@ -45,7 +54,6 @@
                     </div>
                   </div>
                 </div>
-
 
 @endsection
 
