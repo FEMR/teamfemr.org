484fd0cf20cd3ea97127d6ee5eb34538e6facb8f
Ken Dunlap <kdunlap4918@gmail.com>
2017-04-22 11:36:16 -0400

    Removed location seeder since the files are no longer present

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(LocationTablesSeeder::class);
+        //$this->call(LocationTablesSeeder::class);
     }
 }
