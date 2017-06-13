c8796b49b652212bbbb324762a4ba2a1f9e1f721
Ken Dunlap <kdunlap4918@gmail.com>
2017-06-12 23:04:26 -0400

    Re-added proxy middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-//        \Fideloper\Proxy\TrustProxies::class,
+        \Fideloper\Proxy\TrustProxies::class,
     ];
 
     /**
