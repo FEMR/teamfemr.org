8bb6e3653d4a1e1b5599e5558faf2367fe69e8ac
Ken Dunlap <kdunlap4918@gmail.com>
2017-07-02 12:29:53 -0400

    Added news links and images. Other styling tweaks

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,64 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="columns">
-        <div class="column is-half is-offset-one-quarter">
-            <div class="card ">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        Login
-                    </p>
-                </header>
-                <div class="card-content">
-                    <form class="login-form" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
 
-                        <div class="content">
+    <div class="login-box">
 
-                            <div class="field">
-                                <label class="label">Email</label>
-                                <p class="control">
-                                    <input name="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" type="email" value="{{ old('email') }}" required autofocus>
-                                </p>
+        <div class="columns">
+            <div class="column is-half is-offset-one-quarter">
+                <div class="card ">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Login
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <form class="login-form" role="form" method="POST" action="{{ url('/login') }}">
+                            {{ csrf_field() }}
+
+                            <div class="content">
+
+                                <div class="field">
+                                    <label class="label">Email</label>
+                                    <p class="control">
+                                        <input name="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" type="email" value="{{ old('email') }}" required autofocus>
+                                    </p>
+                                </div>
+                                @include('auth.components.form-errors', ['field' => 'email', 'type' => 'horizontal'])
+
+                                <div class="field">
+                                    <label class="label">Password</label>
+                                    <p class="control">
+                                        <input name="password" class="input{{ $errors->has('password') ? ' is-danger' : '' }}" type="password" required>
+                                    </p>
+                                </div>
+                                @include('auth.components.form-errors', ['field' => 'password', 'type' => 'horizontal'])
+
+                                <div class="field">
+                                    <p class="control">
+                                        <label class="checkbox">
+                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            Remember me
+                                        </label>
+                                    </p>
+                                </div>
+
+                                <div class="field is-grouped">
+
+                                    <p class="control">
+                                        <button class="button is-primary">Submit</button>
+                                    </p>
+
+                                    <p class="control is-expanded forgot-password">
+                                        <a href="{{ url('/password/reset') }}">
+                                            Forgot password
+                                        </a>
+                                    </p>
+
+                                </div>
+
+
                             </div>
-                            @include('auth.components.form-errors', ['field' => 'email', 'type' => 'horizontal'])
-
-                            <div class="field">
-                                <label class="label">Password</label>
-                                <p class="control">
-                                    <input name="password" class="input{{ $errors->has('password') ? ' is-danger' : '' }}" type="password" required>
-                                </p>
-                            </div>
-                            @include('auth.components.form-errors', ['field' => 'password', 'type' => 'horizontal'])
-
-                            <div class="field">
-                                <p class="control">
-                                    <label class="checkbox">
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        Remember me
-                                    </label>
-                                </p>
-                            </div>
-
-                            <div class="field is-grouped">
-
-                                <p class="control">
-                                    <button class="button is-primary">Submit</button>
-                                </p>
-
-                                <p class="control is-expanded forgot-password">
-                                    <a href="{{ url('/password/reset') }}">
-                                        Forgot password
-                                    </a>
-                                </p>
-
-                            </div>
-
-
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -7,42 +7,46 @@
         </div>
     @endif
 
-    <div class="columns">
-        <div class="column is-half is-offset-one-quarter">
-            <div class="card ">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        Reset password
-                    </p>
-                </header>
-                <div class="card-content">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {{ csrf_field() }}
+    <div class="login-box">
 
-                        <div class="content">
-                            <div class="control is-horizontal">
-                                <div class="control-label">
-                                    <label class="label">E-mail</label>
+        <div class="columns">
+            <div class="column is-half is-offset-one-quarter">
+                <div class="card ">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Reset password
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                            {{ csrf_field() }}
+
+                            <div class="content">
+                                <div class="control is-horizontal">
+                                    <div class="control-label">
+                                        <label class="label">E-mail</label>
+                                    </div>
+                                    <div class="control is-fullwidth">
+                                        <input name="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" type="email" value="{{ old('email') }}" required autofocus>
+                                    </div>
                                 </div>
-                                <div class="control is-fullwidth">
-                                    <input name="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" type="email" value="{{ old('email') }}" required autofocus>
+                                @include('auth.components.form-errors', ['field' => 'email', 'type' => 'horizontal'])
+
+                                <div class="control is-horizontal">
+                                    <div class="control-label">
+                                        <!-- spacer -->
+                                    </div>
+                                    <div class="control is-fullwidth">
+                                        <button class="button is-primary">Send Password Reset Link</button>
+                                    </div>
                                 </div>
+
                             </div>
-                            @include('auth.components.form-errors', ['field' => 'email', 'type' => 'horizontal'])
-
-                            <div class="control is-horizontal">
-                                <div class="control-label">
-                                    <!-- spacer -->
-                                </div>
-                                <div class="control is-fullwidth">
-                                    <button class="button is-primary">Send Password Reset Link</button>
-                                </div>
-                            </div>
-
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 @endsection
