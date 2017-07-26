d0b4290c2c9264a4a7400da6e2ae81d233fb7461
Ken Dunlap <kdunlap4918@gmail.com>
2017-07-25 22:00:33 -0400

    Temporarily removed the first stages of map info windows and outreach program pages

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 
         Route::get( '/', 'PageController@home' )->name( 'pages.home'  );
 
-        Route::get( '/programs/{program}', 'OutreachProgramController@show' )->name( 'programs.show'  );
+        //Route::get( '/programs/{program}', 'OutreachProgramController@show' )->name( 'programs.show'  );
 
 //        Route::get( '/emr',   'PageController@emr'       )->name( 'pages.emr'   );
 //        Route::get( '/news',  'PageController@news'      )->name( 'pages.news'  );
