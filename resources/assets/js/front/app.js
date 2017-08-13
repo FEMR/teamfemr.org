1bda9c0e8babee230bc7773751f578f6393a3d47
Ken Dunlap <kdunlap4918@gmail.com>
2017-08-13 11:20:48 -0400

    Fixed scrollTo issue on homepage when coming from programs page. Cleaned up the mounted created actions by adding vue instnace  methods

--- resources/assets/js/front/app.js
+++ resources/assets/js/front/app.js
@@ -40,34 +40,64 @@ const app = new Vue({
     },
     methods: {
 
-        
+        /**
+         * Since the homepage is currently a single page, native browser hash scrolling to the
+         *  appropriate sections doesn't take into account the offset from the header. Rather than
+         *  temporarily doing something odd with css, it seems better to just catch the hash with js
+         *  and scroll with the vue-scrollto directive
+         */
+        scrollToHash(){
+
+            // check the current url for a hash tag, if present scroll to this section
+            if( window.location.hash ) {
+
+                // Scroll to the current hash
+                this.$scrollTo( window.location.hash );
+            }
+        },
+
+        /**
+         * Bind the menu hide/show actions
+         *
+         * TODO - vue-ify this code with a component
+         *
+         */
+        setupMobileMenuToggle(){
+
+            // Toggle mobile menu
+            let burger = document.querySelector('.nav-toggle');
+            let menu = document.querySelector('.nav-menu');
+            burger.addEventListener('click', function() {
+
+                burger.classList.toggle('is-active');
+                menu.classList.toggle('is-active');
+            });
+
+            // hide mobile menu when item is clicked
+            let nav = document.querySelector('.nav-right');
+            let nav_items = document.querySelectorAll('a.nav-item');
+            _.forEach( nav_items, ( nav_item ) => {
+
+                nav_item.addEventListener('click', function() {
+
+                    // If is mobile
+                    if( window.innerWidth <= 768 ) {
+
+                        burger.classList.remove('is-active');
+                        menu.classList.remove('is-active');
+                    }
+                });
+            });
+        }
     },
+
+    created() {
+
+        this.scrollToHash();
+    },
+
     mounted(){
 
-        // Toggle mobile menu
-        let burger = document.querySelector('.nav-toggle');
-        let menu = document.querySelector('.nav-menu');
-        burger.addEventListener('click', function() {
-
-            burger.classList.toggle('is-active');
-            menu.classList.toggle('is-active');
-        });
-
-        // hide mobile menu when item is clicked
-        let nav = document.querySelector('.nav-right');
-        let nav_items = document.querySelectorAll('a.nav-item');
-        _.forEach( nav_items, ( nav_item ) => {
-
-            nav_item.addEventListener('click', function() {
-
-                // If is mobile
-                if( window.innerWidth <= 768 ) {
-
-                    burger.classList.remove('is-active');
-                    menu.classList.remove('is-active');
-                }
-            });
-        });
-        
+        this.setupMobileMenuToggle();
     }
 });
