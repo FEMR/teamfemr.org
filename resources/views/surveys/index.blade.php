5efb60afaac1eebcdb5b5d0fa79a0df8649b3235
Ken Dunlap <kdunlap4918@gmail.com>
2017-08-31 21:37:38 -0400

    Added routes, controller, views, component

---
+++ resources/views/surveys/index.blade.php
@@ -0,0 +1,18 @@
+@extends ('layouts.app')
+
+
+@section('content')
+    <div class="survey">
+        <section class="section">
+            <div class="container">
+
+                <h1 class="title">Section</h1>
+
+                <hr>
+
+                <survey></survey>
+
+            </div>
+        </section>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,12 @@
     Route::group( [ 'middleware' => 'web' ], function ()
     {
         Route::get( '/', 'PageController@home' )->name( 'pages.home'  );
-        
+
+        // Outreach Programs
         Route::get( '/programs',           'OutreachProgramController@index' )->name( 'programs.index' );
         Route::get( '/programs/{program}', 'OutreachProgramController@show'  )->name( 'programs.show'  );
+
+        // Surveys
+        Route::get(  '/surveys', 'SurveyController@index' )->name( 'surveys.index' );
+        Route::post( '/surveys', 'SurveyController@store' )->name( 'surveys.store' );
     });
