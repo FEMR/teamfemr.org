fa357008dd4882332b52a582acabb4bec843e7a0
Ken Dunlap <kdunlap4918@gmail.com>
2017-09-06 23:53:59 -0400

    Updated Survey route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@
         Route::get( '/programs/{program}', 'OutreachProgramController@show'  )->name( 'programs.show'  );
 
         // Surveys
-        Route::get(  '/surveys', 'SurveyController@index' )->name( 'surveys.index' );
-        Route::post( '/surveys', 'SurveyController@store' )->name( 'surveys.store' );
+        Route::get(  '/survey', 'SurveyController@index' )->name( 'surveys.index' );
+        Route::post( '/survey', 'SurveyController@store' )->name( 'surveys.store' );
     });
