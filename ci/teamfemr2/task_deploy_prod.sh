805af3d2eed89baa7e82387be7aedc52a69f1eca
Kevin Zurek <kevinzurek@gmail.com>
2017-10-01 13:33:42 -0400

    update deploy script for Concourse

--- ci/teamfemr2/task_deploy_prod.sh
+++ ci/teamfemr2/task_deploy_prod.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-aws ssm send-command --instance-ids "i-002f991384327a0cb" --document-name "AWS-RunShellScript" --comment "TeamFemr2 deploy" --parameters commands=['cd /home/production/git/teamfemr2; git pull origin master; composer install; npm install; npm run prod'] --output json
+aws ssm send-command --instance-ids "i-002f991384327a0cb" --document-name "AWS-RunShellScript" --comment "TeamFemr2 deploy" --parameters commands=['sudo -u production /home/production/update.sh'] --output json
 
 #cp -a git-teamfemr2-app/. teamfemr2-build-folder/
 
