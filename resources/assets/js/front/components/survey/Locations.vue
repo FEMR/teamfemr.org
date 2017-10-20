d4ffb001c541faa68351f6007e00b6c90b6d932d
Ken Dunlap <kdunlap4918@gmail.com>
2017-10-19 21:38:32 -0400

    Removed extra Locations.js model along with some code cleanup

--- resources/assets/js/front/components/survey/Locations.vue
+++ resources/assets/js/front/components/survey/Locations.vue
@@ -6,7 +6,7 @@
             <hr />
 
             <div class="columns">
-                <div class="column">
+                <div class="column map-column">
 
                     <p class="control">
 
@@ -30,7 +30,7 @@
                         <gmap-marker
                                 :key="index"
                                 v-for="(m, index) in locations"
-                                v-if="hasPosition(m)"
+                                v-if="m.hasPosition()"
                                 :position="m.position"
                                 :clickable="true"
                                 :draggable="true"
@@ -110,7 +110,7 @@
 <script type="text/babel">
 
     import * as VueGoogleMaps from 'vue2-google-maps';
-    import Location from '../../models/Location';
+    import VisitedLocation from '../../models/VisitedLocation';
 
     export default {
 
@@ -150,7 +150,7 @@
 
                 if( this.locations.length === 0 ) {
 
-                    this.locations.push( new Location() );
+                    this.locations.push( new VisitedLocation() );
                 }
 
                 this.extendBounds();
@@ -160,11 +160,6 @@
         },
 
         methods: {
-
-            hasPosition( m ) {
-
-              return ! _.isEmpty( m.position );
-            },
 
             editLocation( idx ){
 
@@ -180,16 +175,13 @@
                 // don't add when no location data
                 if( ! _.has( place, [ 'geometry', 'location' ] ) ) return;
 
-                let location = new Location();
+                let location = new VisitedLocation();
                 location.populateFromPlace( place );
 
                 // Since the locations list will always contain at least a blank item
                 //   we need to detect when there is only a single blank item and
                 //   replace that blank item with a new populated Location
-                if( this.locations.length === 1 && _.isEmpty( this.locations[0].position ) )
-                {
-                    console.log( "Set location 0" );
-                    console.log( location );
+                if( this.locations.length === 1 && ! this.locations[0].hasPosition() ) {
 
                     Vue.set( this.locations, 0, location );
                 }
@@ -204,34 +196,26 @@
 
             extendBounds() {
 
-                console.log( "checking bounds" );
-
                 if( this.locations.length > 1 ) {
 
-                    console.log( "Extend Bounds" );
                     const bounds = new google.maps.LatLngBounds();
-
                     _.forEach( this.locations, ( location ) => {
 
-                        //let latLng = new google.maps.LatLng( location.latitude, location.longitude );
                         bounds.extend( location.position );
                     });
-
                     this.$refs.gmap.$mapObject.fitBounds( bounds );
                 }
                 else if( this.locations.length > 0 ) {
 
-                    console.log( "Default Center" );
                     const location = this.locations[0];
-                    if( this.hasPosition( location ) ) {
+                    if( location.hasPosition( location ) ) {
 
                         this.center = location.position;
                         this.zoom = 7;
                     }
                     else {
 
-                        console.log( "No markers" );
-
+                        // default center
                         this.center = { lat: 0.0, lng: 0.0 };
                         this.zoom = 2
                     }
@@ -262,11 +246,23 @@
 
         width: 100%;
         height: 300px;
+
+        //flex: 1;
     }
 
     .locations-map-container .input {
 
         margin-bottom: 5px;
     }
+
+    /*
+    .map-column {
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: stretch;
+    }
+    */
 
 </style>
