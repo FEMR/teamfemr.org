15c546e5cb1a13dde0eaa296bae31869060f3f09
Ken Dunlap <kdunlap4918@gmail.com>
2017-10-19 21:36:55 -0400

    Added clockwork dev tools package

---
+++ app/Http/Controllers/API/OutreachProgramController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FEMR\Http\Controllers\API;
+
+use FEMR\Http\Controllers\Controller;
+use FEMR\Data\Models\City;
+
+class CityController extends Controller
+{
+
+    /**
+     * @return mixed
+     */
+    public function random()
+    {
+        return City::orderByRaw( 'RAND()' )->take( 100 )->get();
+    }
+}
