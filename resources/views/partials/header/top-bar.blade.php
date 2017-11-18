a8dc00fcd0a937be626219fd835ea60d258a1bb2
Ken Dunlap <kdunlap4918@gmail.com>
2017-11-18 01:25:07 -0500

    Added survey link to site

--- resources/views/partials/header/top-bar.blade.php
+++ resources/views/partials/header/top-bar.blade.php
@@ -1,24 +1,9 @@
 <nav id="top-bar" class="top-bar nav">
     <div class="container">
 
-        <div class="nav-left is-hidden-mobile">
-
-            {{--<a class="nav-item" href="http://demo.teamfemr.org"target="_blank">--}}
-                {{--Demo fEMR--}}
-            {{--</a>--}}
-
-            {{--<a class="nav-item" href="{{ asset('/documents/Annual_Report_2014_2015.pdf') }}" target="_blank">--}}
-                {{--Annual Report--}}
-            {{--</a>--}}
-
-        </div>
-
         <div class="nav-right">
 
             <a class="nav-item vcu" href="https://vcu.teamfemr.org/" target="_blank">
-              {{--<span class="icon">--}}
-                {{--<i class="fa fa-facebook-f"></i>--}}
-              {{--</span>--}}
                 VCU
             </a>
 
@@ -42,18 +27,24 @@
 
             @if( Auth::check() )
 
-                {{--@if( Auth::user()->is_admin )--}}
-                {{--<span class="nav-item">--}}
-                  {{--<a  class="button is-info is-small" href="{{ route( 'admin.dashboard.index' ) }}">Admin</a>--}}
-                {{--</span>--}}
-                {{--@endif--}}
-
                 <span class="nav-item is-hidden-mobile logged-in-status">
+
                     Welcome {{ Auth::user()->name }}
+
+
+                    |
+
+                    @if( Auth::user()->is_admin )
+                    <a class="nav-item logged-in-status" href="{{ route( 'admin.dashboard.index' ) }}">
+                      Admin
+                    </a>
+                    @endif
+
                     |
                     {!! Form::open([ 'method' => 'POST', 'route' => 'logout' ]) !!}
-                        <button class="logout-button" href="{{ url('/logout') }}">Logout</button>
+                        <button class="nav-item logout-button" href="{{ url('/logout') }}">Logout</button>
                     {!! Form::close() !!}
+
                 </span>
 
             @else
