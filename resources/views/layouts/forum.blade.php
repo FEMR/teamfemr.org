518d6eef3793f5c88e6b19c0e452faadd41b87f0
Ken Dunlap <kdunlap4918@gmail.com>
2017-11-18 09:03:52 -0500

    Started editing forum templates to use bulma rather than bootstrap classes

--- resources/views/layouts/forum.blade.php
+++ resources/views/layouts/forum.blade.php
@@ -34,11 +34,7 @@
         </div>
 
 
-        <script
-            src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-            crossorigin="anonymous"></script>
-
+        <script src="{{ mix('js/front/forum.js') }}"></script>
         @yield('js')
 
     </body>
