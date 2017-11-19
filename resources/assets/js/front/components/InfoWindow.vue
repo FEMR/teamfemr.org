105539f5c88a2def190c735464336f39ef9dfa77
Ken Dunlap <kdunlap4918@gmail.com>
2017-11-19 13:02:34 -0500

    Fixed months of travel output in info window

--- resources/assets/js/front/components/InfoWindow.vue
+++ resources/assets/js/front/components/InfoWindow.vue
@@ -47,7 +47,7 @@
                 <p class="city_state_country">
                     <span>{{ program.firstLocationCityStateCountry() }}</span>
                 </p>
-                <p><strong>Dates of travel:</strong> {{ program.monthsOfTravel }}</p>
+                <p><strong>Dates of travel:</strong> {{ program.monthsOfTravelList() }}</p>
                 <p><strong>Class Involvement:</strong> {{ ( program.schoolClasses.length > 0 ) ? program.schoolClassesList() : '--' }}</p>
                 <p><strong>Partners:</strong> {{ ( program.partners.length > 0 ) ? program.partnersList() : '--' }}</p>
 
@@ -101,7 +101,6 @@
         },
 
         computed: {
-
 
         },
 
