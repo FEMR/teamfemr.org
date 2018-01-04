53e607021a3002dd75e459c11ee55a5c7a2c6fc0
Ken Dunlap <kdunlap4918@gmail.com>
2018-01-03 21:00:02 -0500

    Forum seeder fixes and content additions. Changed survey welcome message

--- database/seeds/ChatterTableSeeder.php
+++ database/seeds/ChatterTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use FEMR\Data\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,7 +17,7 @@ class ChatterTableSeeder extends Seeder
         $user = User::where( 'email' , '=', 'info@teamfemr.org' )->first();
         if( ! $user )
         {
-            $user = User::create( [ 'name' => 'Team fEMR', 'email' => 'info@teamfemr.org', 'password' => '' ] );
+            $user = User::create( [ 'name' => 'Team fEMR', 'email' => 'info@teamfemr.org', 'password' => '', 'api_token' => str_random( 40 ) ] );
         }
 
 
@@ -75,13 +76,13 @@ class ChatterTableSeeder extends Seeder
              [
                  'id'                  => 1,
                  'chatter_category_id' => 1,
-                 'title'               => 'Welcome!',
+                 'title'               => 'General Discussions',
                  'user_id'             => $user->id,
                  'sticky'              => 0,
                  'views'               => 0,
                  'answered'            => 0,
-                 'created_at'          => '2016-08-18 14:42:29',
-                 'updated_at'          => '2016-08-18 14:42:29',
+                 'created_at'          => Carbon::now(),
+                 'updated_at'          => Carbon::now(),
                  'slug'                => 'welcome',
                  'color'               => '#50a7f0',
              ],
@@ -93,11 +94,37 @@ class ChatterTableSeeder extends Seeder
                 'sticky'              => 0,
                 'views'               => 0,
                 'answered'            => 0,
-                'created_at'          => '2016-08-18 14:42:29',
-                'updated_at'          => '2016-08-18 14:42:29',
+                'created_at'          => Carbon::now(),
+                'updated_at'          => Carbon::now(),
                 'slug'                => 'leaving-feedback',
                 'color'               => '#4dcc70',
-            ]
+            ],
+//             [
+//                 'id'                  => 3,
+//                 'chatter_category_id' => 3,
+//                 'title'               => 'Deploying fEMR',
+//                 'user_id'             => $user->id,
+//                 'sticky'              => 0,
+//                 'views'               => 0,
+//                 'answered'            => 0,
+//                 'created_at'          => Carbon::now(),
+//                 'updated_at'          => Carbon::now(),
+//                 'slug'                => 'deploying-femr',
+//                 'color'               => '#4dcc70',
+//             ],
+//             [
+//                 'id'                  => 4,
+//                 'chatter_category_id' => 4,
+//                 'title'               => 'Research',
+//                 'user_id'             => $user->id,
+//                 'sticky'              => 0,
+//                 'views'               => 0,
+//                 'answered'            => 0,
+//                 'created_at'          => Carbon::now(),
+//                 'updated_at'          => Carbon::now(),
+//                 'slug'                => 'research',
+//                 'color'               => '#4dcc70',
+//             ]
         ]);
 
         // CREATE THE POSTS
@@ -105,22 +132,38 @@ class ChatterTableSeeder extends Seeder
         \DB::table('chatter_post')->delete();
 
         \DB::table('chatter_post')->insert([
-           [
+            [
                'id'                    => 1,
                'chatter_discussion_id' => 1,
                'user_id'               => $user->id,
-               'body'                  => '<p>Welcome to the site, some background info on fEMR, the purpose of the site and why</p>',
-               'created_at' => '2016-08-18 14:42:29',
-               'updated_at' => '2016-08-18 14:42:29',
-           ],
+               'body'                  => '<p>Welcome! This is for general discussion about short term medical relief trips, sustainability, data sharing, etc. Thank you for contributing!</p>',
+               'created_at' => Carbon::now(),
+               'updated_at' => Carbon::now(),
+            ],
             [
                 'id'                    => 2,
                 'chatter_discussion_id' => 2,
                 'user_id'               => $user->id,
-                'body'                  => '<p>General message about leaving feedback</p><ul><li>Create new discussion</li><li>Relevant information that is helpful</li><li>Email or alternative place for feedback</li></ul>',
-                'created_at' => '2016-08-18 14:42:29',
-                'updated_at' => '2016-08-18 14:42:29',
+                'body'                  => '<p>We are eager to hear your feedback! Let us know what you think about this website, the survey, or fEMR.</p><p>If you would like to leave anonymous feedback about fEMR itself, please <a href="https://demo.teamfemr.org/" target="_blank">login to our demo</a></p><ul><li>username: visitor<br></li><li>password: fEMR2016<br></li></ul>',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
+//            [
+//               'id'                    => 3,
+//               'chatter_discussion_id' => 3,
+//               'user_id'               => $user->id,
+//               'body'                  => '<p>Share comments and questions about deploying the fEMR system here.</p>',
+//               'created_at' => Carbon::now(),
+//               'updated_at' => Carbon::now(),
+//            ],
+//            [
+//               'id'                    => 4,
+//               'chatter_discussion_id' => 4,
+//               'user_id'               => $user->id,
+//               'body'                  => '<p>Discussions about research regarding transient healthcare delivery.</p>',
+//               'created_at' => Carbon::now(),
+//               'updated_at' => Carbon::now(),
+//            ],
         ]);
     }
 }
