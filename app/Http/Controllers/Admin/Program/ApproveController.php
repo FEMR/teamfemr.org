bf71ebef3a05857750638350f72a078954ba2c88
Ken Dunlap <kdunlap4918@gmail.com>
2018-09-09 18:36:27 -0400

    Added approve/disapprove actions to outreach programs

--- app/Http/Controllers/Admin/Program/ApproveController.php
+++ app/Http/Controllers/Admin/Program/ApproveController.php
@@ -4,6 +4,7 @@ namespace FEMR\Http\Controllers\Admin\Program;
 
 use Carbon\Carbon;
 use FEMR\Data\Models\OutreachProgram;
+use FEMR\Data\Repositories\OutreachProgramRepository;
 use FEMR\Http\Controllers\Controller;
 
 class ApproveController extends Controller
@@ -11,14 +12,14 @@ class ApproveController extends Controller
     /**
      * @param $outreach_program_id
      *
+     * @param OutreachProgramRepository $repository
+     *
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store( $outreach_program_id )
+    public function store( $outreach_program_id, OutreachProgramRepository $repository )
     {
-        $program = OutreachProgram::findOrFail( $outreach_program_id );
-        $program->approved_by = auth()->user()->id;
-        $program->approved_at = Carbon::now();
-        $program->save();
+        $program = $repository->findOrFail( $outreach_program_id );
+        $repository->approve($program);
 
         return redirect()->route('admin.programs.index' )
                          ->with( 'message', [ 'type' => 'is-success', 'message' => $program->name . ' was Approved' ] );
@@ -27,14 +28,15 @@ class ApproveController extends Controller
     /**
      * @param $outreach_program_id
      *
+     * @param OutreachProgramRepository $repository
+     *
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy( $outreach_program_id )
+    public function destroy($outreach_program_id, OutreachProgramRepository $repository)
     {
-        $program = OutreachProgram::findOrFail( $outreach_program_id );
-        $program->approved_by = null;
-        $program->approved_at = null;
-        $program->save();
+        $user_id = auth()->user()->id;
+        $program = $repository->findOrFail($outreach_program_id, $user_id);
+        $repository->disapprove($program);
 
         return redirect()->route( 'admin.programs.index' )
                          ->with( 'message', [ 'type' => 'is-success', 'message' => $program->name . ' was Unapproved' ] );
