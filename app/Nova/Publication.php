7a61af282c4099623022b00e1fbebdde86995005
Ken Dunlap <kdunlap4918@gmail.com>
2018-09-09 21:40:20 -0400

    Fixed issues with news and publications

--- app/Nova/Publication.php
+++ app/Nova/Publication.php
@@ -50,6 +50,10 @@ class Publication extends Resource
                 ->sortable()
                 ->rules('required', 'max:255'),
 
+            Text::make('Slug')
+                ->rules('max:255')
+                ->onlyOnForms(),
+
             Text::make('Description')
                 ->rules('required', 'max:255'),
 
