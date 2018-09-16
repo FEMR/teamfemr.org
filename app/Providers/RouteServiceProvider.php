9e40aebaeddbe78797386925c8947c1980c9b950
Ken Dunlap <kdunlap4918@gmail.com>
2018-09-16 18:19:44 -0400

    Removed old admin files

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,26 +36,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
         $this->mapWebRoutes();
-        $this->mapAdminRoutes();
         //
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapAdminRoutes()
-    {
-        Route::group([
-            'middleware' => 'admin',
-            'prefix'    => 'admin',
-            'namespace' => $this->namespace . '\Admin',
-        ], function ($router) {
-            require base_path('routes/admin.php');
-        });
     }
 
     /**
