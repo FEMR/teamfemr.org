f34c9596479aa844d0575902f1fc739244935ae7
Ken Dunlap <kdunlap4918@gmail.com>
2019-02-18 14:14:49 -0500

    Forced prod and staging to generate https urls only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if( $this->app->environment('production') || $this->app->environment('staging') )
+        {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
