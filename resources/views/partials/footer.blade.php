466167e8d13cc6c48d9ac9590d8b00e48e9a1823
Ken Dunlap <kdunlap4918@gmail.com>
2020-11-23 12:33:59 -0500

    Added HIPAA logo on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,12 +3,20 @@
 
         <div class="columns">
 
-            <div class="column">
+            <div class="column copyright">
 
                 <p class="copy">Copyright &copy; {{ date( 'Y' ) }} - Team fEMR <br class="is-hidden-desktop" /> Designed by <a href="https://www.ciaradiamond.com" target="_blank">Ciara Diamond</a></p>
 
                 <p class="copy">Team fEMR is a 501(c)(3) nonprofit organization</p>
 
+
+
+            </div>
+
+            <div class="column">
+                <a href="https://compliancy-group.com/hipaa-compliance-verification" target="_blank" rel="noopener">
+                    <img class="aligncenter wp-image-13813" src="https://compliancy-group.com/wp-content/uploads/2020/07/HIPAA-Website-Verified-Seal.png" alt="HIPAA Seal of Compliance” width=" width="150" />
+                </a>
             </div>
 
             <div class="column">
